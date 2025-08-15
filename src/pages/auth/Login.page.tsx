98f71d480e53fe4f6d88f6b0b73085640ae80b47
Alcides <alcidescon@gmail.com>
2025-08-15 09:44:39 +0200

    card chadcn

--- src/pages/auth/Login.page.tsx
+++ src/pages/auth/Login.page.tsx
@@ -1,5 +1,40 @@
+import { Button } from "@/components/ui/button";
+import { useAuthActions } from "../../hooks/useAuthActions";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export const LoginPage = () => {
+  const { loginWithGoogle } = useAuthActions();
+
+  const handleLoginWithGoogle = async () => {
+    const result = await loginWithGoogle();
+    if(result.success){
+      console.log("Login Succesful");
+    }else {
+      console.error("Login failed", result.error);
+    }
+  }
+
   return (
-	<div>LoginPage</div>
-  )
-}
+    <Card>
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>
+          Login to your account using Email and password or with Google.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>...</CardContent>
+      <CardFooter>
+        <Button onClick={handleLoginWithGoogle} className="w-full">
+          Login with Google
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
